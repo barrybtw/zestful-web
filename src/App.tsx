f6e7a93a7527d62fff22a421aa0a2f7587e64241
Nicolai Christensen <nicolaibernhardchristensen@gmail.com>
2023-12-19 21:09:34 +0100

    feat: add some components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { Button } from "./components/ui/button";
 import { ThemeProvider } from "@/components/theme-provider";
+import { useToast } from "./components/ui/use-toast";
 
 function App() {
+  const { toast } = useToast();
   return (
     <ThemeProvider defaultTheme="system" storageKey="zestful-ui-theme">
-      <Button>Hello this is</Button>
+      <Button
+        onClick={() => {
+          toast({
+            title: "Hello",
+            description: "This is a test toast!",
+            variant: "success",
+          });
+        }}
+      >
+        Hello this is a test toast!
+      </Button>
     </ThemeProvider>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import { Toaster } from "@/components/ui/toaster";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
+    <Toaster />
   </React.StrictMode>
 );
